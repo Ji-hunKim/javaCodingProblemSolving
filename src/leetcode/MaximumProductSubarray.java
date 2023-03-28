a65cd23628a903bcb3ded8facc396bf34789c3e2
Jihun-Kim <anam4561@naver.com>
2023-03-28 21:11:29 +0900

    20230328

---
+++ src/leetcode/MaximumProductSubarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(vector<int>& nums) {
+        int maxProducts = nums[0];
+        int minProducts = nums[0];
+        int ans = nums[0];
+        for(int i=1;i<nums.size();i++){
+            int now = nums[i];
+            int a = maxProducts * now;
+            int b = minProducts * now;
+            maxProducts = max(now, max(a, b));
+            minProducts = min(now, min(a, b));
+            ans = max(ans, maxProducts);
+        }
+        return ans;
+    }
+};
