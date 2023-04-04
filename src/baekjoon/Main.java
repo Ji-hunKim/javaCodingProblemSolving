b6e1ad12d432c30a743afc1bdf3abf76f2ac4226
Jihun-Kim <anam4561@naver.com>
2023-04-05 01:59:35 +0900

    20230405

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,64 +3,55 @@ package baekjoon;
 import java.util.*;
 
 class Main {
-    static int N,K;
-    static int max = 0;
-    static boolean visit[] = new boolean[26];
-    static String[] arr;
+    static int n,k;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        K = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
 
-        arr = new String[N];
+       List<Integer> orders = new ArrayList<>();
+       List<Integer> multitap = new ArrayList<>();
 
-        if(K<5) {
-            System.out.println(0);
-            return;
-        }else if(K==26) {
-            System.out.println(N);
-            return;
-        }else {
-            for (int i = 0; i < N; i++) {
-                String str = scanner.next();
-                arr[i] = str.substring(4, str.length() - 4);
-            }
-            K -= 5;
-            visit['a' - 97] = true;
-            visit['n' - 97] = true;
-            visit['t' - 97] = true;
-            visit['i' - 97] = true;
-            visit['c' - 97] = true;
-            dfs(0, 0);
-            System.out.println(max);
+        int answer = 0;
+        for(int i = 0; i<k; i++){
+            orders.add(sc.nextInt());
         }
-    }
-
-    private static void dfs(int start, int count) {
-        if(count == K) {
-            int rs = 0;
-            for(int i=0; i<N; i++) {
-                boolean isTrue = true;
-                for(int j=0; j<arr[i].length(); j++) {
-                    if(!visit[arr[i].charAt(j)-97]) {
-                        isTrue = false;
+        while(!orders.isEmpty()){
+            int order = orders.remove(0);
+            // 멀티탭에서 이미 사용중인 경우
+            if(multitap.contains(order)) continue;
+            // 멀티탭에 자리가 있는 경우
+            if(multitap.size() < n){
+                multitap.add(order);
+            } else {
+                // 멀티탭에 자리가 없는 경우
+                boolean flag = false;
+                int idx = -1;
+                int maxIdx = -1;
+                answer++;
+                for(int i = 0; i<multitap.size(); i++){
+                    if(!orders.contains(multitap.get(i))){
+                        // 다시 사용하지 않을 전자기기인 경우
+                        flag = true;
+                        multitap.remove(i);
+                        multitap.add(order);
                         break;
+                    } else {
+                        if(orders.indexOf(multitap.get(i))> idx){
+                            // 다시 사용할 전자기기의 우선순위를 계산
+                            idx = orders.indexOf(multitap.get(i));
+                            maxIdx = i;
+                        }
                     }
                 }
-                if(isTrue) {
-                    rs++;
+                if(!flag){
+                    // 모든 전자기기가 다시 사용할 전자기기인 경우
+                    // 계산한 우선순위를 이용해서 해결
+                    multitap.remove(maxIdx);
+                    multitap.add(order);
                 }
             }
-            max = Math.max(max, rs);
-            return;
         }
-
-        for(int i=start; i<26; i++) {
-            if(!visit[i]) {
-                visit[i]=true;
-                dfs(i, count+1);
-                visit[i]=false;
-            }
-        }
+        System.out.println(answer);
     }
 }
