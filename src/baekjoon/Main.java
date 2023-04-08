0bf4a39760b2d15e7476d0bf92013d39f77fb6f6
Jihun-Kim <anam4561@naver.com>
2023-04-09 00:43:00 +0900

    20230409

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
+
         N = sc.nextInt();
         sum = sc.nextInt();
 
