8fa4c5dc82e15857d7ddbd519cf4be3adbbc9ece
Jihun-Kim <anam4561@naver.com>
2023-04-22 00:23:08 +0900

    20230422

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,30 +3,62 @@ package baekjoon;
 import java.util.*;
 
 public class Main {
-    static int n;
+    static int S;
+    static int minVal = 2000;
+    static int[] visited = new int[2000];
+    static int[] copied = new int[2000];
+    static boolean ch = false;
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        n = s.nextInt();
-        int list[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                list[i][j] = s.nextInt();
+        S = sc.nextInt();
+        Arrays.fill(visited, -1);
+        bfs();
+
+        System.out.println(minVal);
+    }
+
+    public static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{1, 0, 0});
+        visited[1] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int cur = current[0];
+            int count = current[1];
+            int prev = current[2];
+
+            if (cur == S) {
+                minVal = Math.min(minVal, count);
+            } else if (cur > S) {
+                if (visited[cur - 1] == -1 || visited[cur - 1] >= count + 1) {
+                    if (cur - 1 > 0) {
+                        visited[cur - 1] = count + 1;
+                        queue.offer(new int[]{cur - 1, count + 1, prev});
+                    }
+                }
+            } else {
+                if(copied[cur] == 0){
+                    copied[cur] = 1;
+                    queue.offer(new int[]{cur, count + 1, cur});
+                }
+
+                if (visited[cur + prev] == -1 || visited[cur + prev] >= count + 1) {
+                    if (cur + prev <= 2000 && prev != 0) {
+                        visited[cur + prev] = count + 1;
+                        queue.offer(new int[]{cur + prev, count + 1, prev});
+                    }
+                }
+
+                if (visited[cur - 1] == -1 || visited[cur - 1] >= count + 1) {
+                    if (cur - 1 > 0) {
+                        visited[cur - 1] = count + 1;
+                        queue.offer(new int[]{cur - 1, count + 1, prev});
+                    }
+                }
             }
         }
-
-        long dp[][] = new long[n][n];
-        dp[0][0] = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int next = list[i][j];
-                if (next == 0) break;
-                if (j + next < n) dp[i][j + next] += dp[i][j];
-                if (i + next < n) dp[i + next][j] += dp[i][j];
-            }
-        }
-        System.out.println(dp[n-1][n-1]);
     }
 }
