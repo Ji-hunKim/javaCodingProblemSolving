da8d911684f144084b981580162647a46e21349a
Jihun-Kim <anam4561@naver.com>
2023-06-05 21:05:21 +0900

    20230605

--- src/baekjoon/센서.java
+++ src/baekjoon/센서.java
@@ -1,8 +1,12 @@
 package baekjoon;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class 센서 {
+
     static int N,K;
     static int cnt;
 
