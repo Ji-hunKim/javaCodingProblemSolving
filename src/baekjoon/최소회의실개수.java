ed43eb831125d7d7aa7d132b06edfce51615ee5b
Jihun-Kim <anam4561@naver.com>
2023-06-05 20:15:03 +0900

    20230605

--- src/baekjoon/최소회의실개수.java
+++ src/baekjoon/최소회의실개수.java
@@ -1,8 +1,13 @@
 package baekjoon;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
-public class Main {
+public class 최소회의실개수 {
+
+
     static int N;
     static int arr[][];
     static int cnt;
