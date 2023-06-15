fe47bf3532150fc970cad5d871016e78a11dd2bd
Jihun-Kim <anam4561@naver.com>
2023-06-15 22:06:29 +0900

    20230615

--- src/baekjoon/택배.java
+++ src/baekjoon/택배.java
@@ -1,8 +1,11 @@
 package baekjoon;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
-public class Main {
+public class 택배 {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
