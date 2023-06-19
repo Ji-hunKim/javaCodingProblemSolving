3d9e625be61055adb86b710026c5ad405e079207
Jihun-Kim <anam4561@naver.com>
2023-06-19 20:28:06 +0900

    20230619

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -1,36 +1,80 @@
 package baekjoon;
 
 import java.util.*;
-public class Main {
-    static int K;
-    static String S, T;
-    static int result;
+
+class Main {
+    static int n;
+
+    static boolean[] arr1, arr2, goal;
+
+    static int cnt1 = 1;
+    static int cnt2;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        S = sc.next();
-        T = sc.next();
-        K = T.length();
-        dfs(T);
-        System.out.println(result);
+        n = sc.nextInt();
+        arr1 = new boolean[n];
+        arr2 = new boolean[n];
+        goal = new boolean[n];
+
+        char[] temp;
+        temp = sc.next().toCharArray();
+        for (int i = 0; i < n; i++) {
+            arr1[i] = temp[i] == '1';
+            arr2[i] = temp[i] == '1';
+        }
+        temp = sc.next().toCharArray();
+
+        for (int i = 0; i < n; i++) {
+            goal[i] = temp[i] == '1';
+        }
+
+
+        swithOn(arr1, 0);
+
+        for (int i = 1; i < n; i++) {
+            if (arr1[i - 1] != goal[i - 1]) {
+                swithOn(arr1, i);
+                cnt1++;
+            }
+            if (arr2[i - 1] != goal[i - 1]) {
+                cnt2++;
+                swithOn(arr2, i);
+            }
+        }
+
+        int ret = checkAndReturn();
+        System.out.println(ret == Integer.MAX_VALUE ? -1 : ret);
+
+
     }
 
-    public static void dfs(String t) {
-        if (S.length() == t.length()) {
-            if (S.equals(t)) {
-                result = 1;
+
+    static void swithOn(boolean[] arr, int idx) {
+        for (int i = idx - 1; i <= idx + 1; i++) {
+            if (i >= 0 && i < n) {
+                arr[i] = !arr[i];
             }
-            return;
         }
-        if (t.charAt(0) == 'B') {
-            String substring = t.substring(1);
-            StringBuilder sb = new StringBuilder(substring);
-            String string = sb.reverse().toString();
-            dfs(string);
+    }
+
+    static int checkAndReturn() {
+        int ret = Integer.MAX_VALUE;
+        boolean ok1 = true;
+        boolean ok2 = true;
+
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != goal[i]) {
+                ok1 = false;
+            }
+            if (arr2[i] != goal[i]) {
+                ok2 = false;
+            }
         }
 
-        if (t.charAt(t.length() - 1) == 'A') {
-            dfs(t.substring(0, t.length() - 1));
-        }
+        if (ok1) ret = cnt1;
+        if (ok2) ret = Math.min(ret, cnt2);
+
+        return ret;
     }
 }
