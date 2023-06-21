c36250e05cbda766dcc02d40c314503dda9cac6d
Jihun-Kim <anam4561@naver.com>
2023-06-21 20:39:31 +0900

    20230621

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -3,75 +3,74 @@ package baekjoon;
 import java.util.*;
 
 public class Main {
+    static int n;					// n 개 장소
+    static int[] honeys;			// 각 장소의 꿀 양
+    static long maxCount;			// 출력: 최대 꿀 양
+
+    static long total;				// 모든 장소들의 꿀 양 합
+    static long[] toRightTotal;		// [0 ~ 벌 2 위치] 누적합
+    static long[] toLeftTotal;		// [끝 ~ 벌 2 위치] 누적합
+
+    /* 벌통: 맨 오른쪽 고정, 벌 1: 맨 왼쪽 고정 => 벌 2의 위치 선택 */
+    static void case1() {
+        long bee1, bee2;			// 벌 1, 벌 2의 꿀 채집량
+
+        for (int i = 1; i <= n - 2; i++) {
+            bee1 = total - honeys[0] - honeys[i];
+            bee2 = total - toRightTotal[i];
+            maxCount = Math.max(maxCount, bee1 + bee2);
+        }
+    }
+
+    /* 벌통: 맨 왼쪽 고정, 벌 1: 맨 오른쪽 고정 => 벌 2의 위치 선택 */
+    static void case2() {
+        long bee1, bee2;
+
+        for (int i = n - 2; i >= 1; i--) {
+            bee1 = total - honeys[n - 1] - honeys[i];
+            bee2 = total - toLeftTotal[i];
+            maxCount = Math.max(maxCount, bee1 + bee2);
+        }
+    }
+
+    /* 벌 1: 맨 왼쪽 고정, 벌 2: 맨 오른쪽 고정 => 벌통: 벌 사이에서 선택 */
+    static void case3() {
+        long bee1, bee2;
+
+        for (int i = 1; i <= n - 2; i++) {
+            bee1 = toRightTotal[i] - honeys[0];
+            bee2 = toLeftTotal[i] - honeys[n - 1];
+            maxCount = Math.max(maxCount, bee1 + bee2);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String balls = sc.next();
-        int cnt = Integer.MAX_VALUE;
+        n = sc.nextInt();
 
+        honeys = new int[n];
+        toRightTotal = new long[n];			// 왼쪽 -> 오른쪽으로 꿀 양 누적합
+        toLeftTotal = new long[n];			// 오른쪽 -> 왼쪽으로 꿀 양 누적합
+        long temp = 0;
+        for (int i = 0; i < n; i++) {
+            honeys[i] = sc.nextInt();
 
-        // 왼쪽으로 빨간 공 모으기
-
-        int subCnt = 0;
-        boolean sw = false;
-        for (int i = 0; i < N; i++) {
-            if (sw && balls.charAt(i) == 'R') {
-                subCnt++;
-                continue;
-            }
-
-            if (balls.charAt(i) == 'B') sw = true;
-        }
-        cnt = Math.min(cnt, subCnt);
-
-        // 왼쪽으로 파란 공 모으기
-
-        subCnt = 0;
-        sw = false;
-        for (int i = 0; i < N; i++) {
-            if (sw && balls.charAt(i) == 'B') {
-                subCnt++;
-                continue;
-            }
-
-            if (balls.charAt(i) == 'R') sw = true;
-        }
-        cnt = Math.min(cnt, subCnt);
-
-
-        // 오른쪽으로 빨간 공 모으기
-
-        subCnt = 0;
-        sw = false;
-        for (int i = N - 1; i >= 0; i--) {
-            if (sw && balls.charAt(i) == 'R') {
-                subCnt++;
-                continue;
-            }
-
-            if (balls.charAt(i) == 'B') sw = true;
-        }
-        cnt = Math.min(cnt, subCnt);
-
-        // 오른쪽으로 파란 공 모으기
-
-        subCnt = 0;
-        sw = false;
-        for (int i = N - 1; i >= 0; i--) {
-            if (sw && balls.charAt(i) == 'B') {
-                subCnt++;
-                continue;
-            }
-
-            if (balls.charAt(i) == 'R') sw = true;
+            temp += honeys[i];
+            toRightTotal[i] = temp;
         }
 
-        cnt = Math.min(cnt, subCnt);
+        temp = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            temp += honeys[i];
+            toLeftTotal[i] = temp;
+        }
 
+        total = toRightTotal[n - 1];
 
-        System.out.println(cnt);
+        case1();
+        case2();
+        case3();
 
-
+        System.out.println(maxCount);
     }
 }
