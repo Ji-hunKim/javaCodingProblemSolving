4dcaf3164472d15d210b298653118dd54116a1ec
Jihun-Kim <anam4561@naver.com>
2023-07-20 22:33:52 +0900

    20230720

--- src/leetcode/FirstUniqueCharacterInAString.java
+++ src/leetcode/FirstUniqueCharacterInAString.java
@@ -2,28 +2,30 @@ package leetcode;
 
 class Solution {
     public int firstUniqChar(String s) {
-
-        // 초기 세팅
-        int[] alpa = new int[26];
-        Arrays.fill(alpa, -1);
-
-        // 알파벳의 위치를 파악하고 중복이면 MAX 값을 넣어준다.
-        for(int i=0; i<s.length(); i++){
-            int n = s.charAt(i) - 'a';
-            if(alpa[n] == -1){
-                alpa[n] = i;
-            }else if(alpa[n] > -1){
-                alpa[n] = Integer.MAX_VALUE;
+        int[] alph = new int[26];
+        //char -> first index pairs
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //count char occurences
+            alph[c - 'a']++;
+            //this if statement is used for optimization
+            //if keySet().size() = 26, then we no longer
+            //call map's methods
+            if (map.keySet().size() < 26) {
+                if (!map.containsKey(c)) {
+                    map.put(c, i);
+                }
             }
         }
-        // 존재하는 알파벳 중에 최소 값을 찾는다.
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<26 ; i++){
-            if(alpa[i] != -1) min = Math.min(min, alpa[i]);
+        int minIdx = s.length();
+        //finding minimum index of char, whose occurence is 1.
+        for (char c : map.keySet()) {
+            if (alph[c - 'a'] == 1) {
+                int idx = map.get(c);
+                if (minIdx > idx) minIdx = idx;
+            }
         }
-
-        // 만약, 1개인 알파벳이 존재하지않으면 -1 출력.
-        if(min == Integer.MAX_VALUE) min = -1;
-        return min;
+        return minIdx == s.length() ? -1 : minIdx;
     }
 }
