b197752da9c5fc0850c36b81012c8824c4c65f0f
Jihun-Kim <anam4561@naver.com>
2023-08-16 20:20:28 +0900

    20230816

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -4,73 +4,105 @@ import java.util.*;
 
 public class Main {
 
-    static int N,M;
+    static int N,L,R,cnt,answer;
     static int[][] map;
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    static int maxVal = -1;
-    static boolean flag = false;
+    static boolean[][] visited;
+    static int[] dx = {0,1};
+    static int[] dy = {1,0};
+    static List<Integer> li = new ArrayList<>();
+
+
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        L = sc.nextInt();
+        R = sc.nextInt();
 
-        M = scan.nextInt();
-        N = scan.nextInt();
-
-        map = new int[N][M];
-
-        Queue<Node> queue = new LinkedList<>();
+        map = new int[N][N];
 
         for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                map[i][j] = scan.nextInt();
-                if(map[i][j] == 1){
-                    queue.offer(new Node(i,j));
-                }else if(map[i][j] == 0){
-                    flag = true;
-                }
+            for(int j=0; j<N; j++){
+                map[i][j] = sc.nextInt();
             }
         }
-
-        if(!flag) maxVal = 1;
-        else{
-            while(!queue.isEmpty()){
-                Node current = queue.poll();
-
-                for(int i=0; i<4; i++){
-                    int nx = current.x + dx[i];
-                    int ny = current.y + dy[i];
-
-                    if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
-
-                    if(map[nx][ny] != 0) continue;
-
-                    map[nx][ny] = map[current.x][current.y] + 1;
-                    queue.offer(new Node(nx,ny));
-                }
-            }
-
-            Loop1:
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    if(map[i][j] == 0){
-                        maxVal = 0;
-                        break Loop1;
-                    }
-                    maxVal = Math.max(map[i][j], maxVal);
-                }
-            }
-        }
-        System.out.println(maxVal-1);
+        answer = 0;
+        bfs();
+        System.out.println(answer);
     }
 
+    private static void bfs(){
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(0,0, false));
 
-    public static class Node {
-        int x,y;
+        while(true){
+            cnt = 0;
+            visited = new boolean[N][N];
 
-        public Node(int x, int y) {
+            while(!queue.isEmpty()){
+                Node now = queue.poll();
+                int ix = now.x;
+                int iy = now.y;
+
+                visited[ix][iy] = true;
+
+                for(int i=0; i<2; i++){
+                    int nx = ix + dx[i];
+                    int ny = iy + dy[i];
+
+                    if(nx >= N || ny >= N) continue;
+
+                    if(visited[nx][ny]) continue;
+
+                    if(Math.abs(map[ix][iy] - map[nx][ny]) >= L && Math.abs(map[ix][iy] - map[nx][ny]) <= R){
+                        if(!now.ch){
+                            li.add(map[ix][iy]);
+                            li.add(map[nx][ny]);
+                            cnt += 2;
+                        }else{
+                            li.add(map[nx][ny]);
+                            cnt++;
+                        }
+                        queue.offer(new Node(nx,ny,true));
+                    }else{
+                        queue.offer(new Node(nx,ny,false));
+                    }
+                }
+            }
+            if(cnt == 0) return;
+
+            int sum = 0;
+            int val = 0;
+
+            for(int i=0; i<li.size(); i++){
+                sum += li.get(i);
+            }
+
+            val = sum / cnt;
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(visited[i][j]){
+                        map[i][j] = val;
+                    }
+                }
+            }
+            answer++;
+            li.clear();
+        }
+    }
+
+    private static class Node{
+        int x;
+        int y;
+        boolean ch;
+
+        public Node(int x, int y, boolean ch) {
             this.x = x;
             this.y = y;
+            this.ch = ch;
         }
     }
+
+
 }
