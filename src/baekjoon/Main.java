1aff80b0040fb67615db8234e47431bd9cb96a1d
Jihun-Kim <anam4561@naver.com>
2023-11-17 21:57:30 +0900

    20231117

--- src/baekjoon/Main.java
+++ src/baekjoon/Main.java
@@ -6,6 +6,41 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+
+public class Main {
+    static int N;
+    static int sum;
+    static int[] coins;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        sum = sc.nextInt();
+
+        coins = new int[N];
+
+        for(int i=0; i<N; i++){
+            coins[i] = sc.nextInt();
+        }
+
+        dp = new int[sum+1];
+        dp[0] = 1;
+
+        for(int i = 0; i < N; i++) {
+            for(int j = coins[i]; j <= sum; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+
+        System.out.println(dp[sum]);
+
+    }
+}
+
+
+
 public class Main {
     static int N;
     static int[] num;
